692c4a3e6cdd4fa76d4d503f528a5cd7c14e82c6
DenisGalesNeves <125305296+DenisGalesNeves@users.noreply.github.com>
2024-05-13 18:03:57 -0300

    Add files via upload

---
+++ atividade/demo/src/main/java/br/fatec/departamento/demo/DemoApplication.java
@@ -0,0 +1,10 @@
+package br.fatec.departamento.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements Comma {
+	}
+
+}
